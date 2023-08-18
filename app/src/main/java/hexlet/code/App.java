ae104babd266e6f497b7143cf23125c32950344c
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-08-18 14:47:22 +0300

    Step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,11 @@
 package hexlet.code;
+import java.util.Scanner;
+
+import static hexlet.code.Cli.greeting;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        greeting();
     }
 }
 
