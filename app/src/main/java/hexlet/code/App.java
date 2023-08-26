a11180137305cca915011d8bc0b5e0a14d5a31a6
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-08-26 21:29:29 +0300

    enhanced switch

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,37 +10,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit");
+                + "1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
         System.out.println("Your choice: " + choice);
         switch (choice) {
-            case 1:
-                Cli.greeting();
-                break;
-            case 2:
-                Engine.startGame(new Even());
-                break;
-            case 3:
-                Engine.startGame(new Calc());
-                break;
-            case 4:
-                Engine.startGame(new Gcd());
-                break;
-            case 5:
-                Engine.startGame(new Progression());
-                break;
-            case 6:
-                Engine.startGame(new Prime());
-                break;
-            default:
-                break;
+            case 1 -> Cli.greeting();
+            case 2 -> Engine.startGame(new Even());
+            case 3 -> Engine.startGame(new Calc());
+            case 4 -> Engine.startGame(new Gcd());
+            case 5 -> Engine.startGame(new Progression());
+            case 6 -> Engine.startGame(new Prime());
         }
     }
 }
