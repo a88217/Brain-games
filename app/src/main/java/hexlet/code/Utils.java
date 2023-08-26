08a5ab5c201deb20ba5f2d9a13c7ac743e8fa9f4
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-08-26 20:45:27 +0300

    cosmetic

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Utils {
     public static int getRandom(int max) {
         int random = (int) (Math.random() * max) + 1;
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -8,24 +8,23 @@ public class Progression implements Game {
         return gameRules;
     }
     public String[] getGameData() {
-        final int minProgressionLength = 5; // Минимальная длина прогрессии
-        final int maxProgressionLength = 10; // Максимальная длина прогрессии
-        final int maxProgressionStep = 10; // Максимальный шаг
-        final int maxStartNumber = 20; // Максимальное стартовое число прогрессии
-        int progressionLength = (int) (Math.random() * maxProgressionLength) + minProgressionLength; // Генерируем рандомную длину прогрессии в пределах заданного константами диапазона
-        int missingNumber = Utils.getRandom(progressionLength); // Генерируем место пропущенного элемента в прогрессии
-        int startNumber = Utils.getRandom(maxStartNumber); // Генерируем стартовое число прогрессии
-        int nextNumber = startNumber; // инициализируем переменную для следующего числа прогрессии
-        int progressionStep = Utils.getRandom(maxProgressionStep); // Генерируем шаг прогрессии
-        int correctAnswer = 1; // инициализируем переменную для вычисления правильного ответа
-        String question = "" + startNumber; // инициализируем переменную для формирования строки вопроса и присваиваем ей значение стартового числа в виде строки
+        final int minProgressionLength = 5;
+        final int maxProgressionLength = 10;
+        final int maxProgressionStep = 10;
+        final int maxStartNumber = 20;
+        int progressionLength = (int) (Math.random() * maxProgressionLength) + minProgressionLength;
+        int missingNumber = Utils.getRandom(progressionLength);
+        int startNumber = Utils.getRandom(maxStartNumber);
+        int nextNumber = startNumber;
+        int progressionStep = Utils.getRandom(maxProgressionStep);
+        int correctAnswer = 1;
+        String question = "" + startNumber;
         if (missingNumber == 1) {
             question = "..";
             correctAnswer = startNumber;
-        } // Для случая когда позиция пропущенного элемента - первая - присваиваем строке с ответом стартовое значение ".."
-          // А правильному ответу присваиваем значение стартового числа
-        var result = new StringBuilder(question); // объект стрингбилдер для формирования строки с ответом
-        for (int i = 2; i <= progressionLength; i++) { // Цикл для формирования строки и вычисления правильного ответа
+        }
+        var result = new StringBuilder(question);
+        for (int i = 2; i <= progressionLength; i++) {
             if (i == missingNumber) {
                 result.append(" ..");
                 nextNumber += progressionStep;
@@ -35,7 +34,7 @@ public class Progression implements Game {
                 result.append(" " + nextNumber);
             }
         }
-        String stringQuestion = result.toString(); // Перевод объекта стрингбилдер в строку ответа
+        String stringQuestion = result.toString();
         String[] gameData = {stringQuestion, Integer.toString(correctAnswer)};
         return gameData;
     }
