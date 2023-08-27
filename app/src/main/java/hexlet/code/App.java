838e4d0b93b9f14e27f9e1f3973903d503b8149f
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-08-27 10:42:59 +0300

    constants

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,21 +11,21 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
-        final int EVEN = 2;
-        final int CALC = 3;
-        final int GCD = 4;
-        final int PROGRESSION = 5;
-        final int PRIME = 6;
+        final int even = 2;
+        final int calc = 3;
+        final int gcd = 4;
+        final int progression = 5;
+        final int prime = 6;
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
         System.out.println("Your choice: " + choice);
         switch (choice) {
             case 1 -> Cli.greeting();
-            case EVEN -> Engine.startGame(new Even());
-            case CALC -> Engine.startGame(new Calc());
-            case GCD -> Engine.startGame(new Gcd());
-            case PROGRESSION -> Engine.startGame(new Progression());
-            case PRIME -> Engine.startGame(new Prime());
+            case even -> Engine.startGame(new Even());
+            case calc -> Engine.startGame(new Calc());
+            case gcd -> Engine.startGame(new Gcd());
+            case progression -> Engine.startGame(new Progression());
+            case prime -> Engine.startGame(new Prime());
             default -> System.out.println("Exit");
         }
     }
