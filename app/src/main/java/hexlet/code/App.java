1d528bae30f20b64e1579a5a17875618bee7d339
Aleksandr Muzalev <muzalev.as@gmil.com>
2023-08-30 17:33:03 +0300

    constants in class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,12 +7,12 @@ import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 public class App {
-    public static final Integer GREET = 1;
-    public static final Integer EVEN = 2;
-    public static final Integer CALC = 3;
-    public static final Integer GCD = 4;
-    public static final Integer PROGRESSION = 5;
-    public static final Integer PRIME = 6;
+    public static final String GREET = "1";
+    public static final String EVEN = "2";
+    public static final String CALC = "3";
+    public static final String GCD = "4";
+    public static final String PROGRESSION = "5";
+    public static final String PRIME = "6";
         public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
@@ -22,7 +22,7 @@ public class App {
                 + "5 - Progression\n"
                 + "6 - Prime\n0 - Exit");
         Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+        String choice = scanner.next();
         System.out.println("Your choice: " + choice);
         switch (choice) {
             case GREET -> Engine.greeting();
